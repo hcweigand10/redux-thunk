387d4a047c735e78cc2ad520b59923e357eb0753
hcweigand10 <henryweigand10@gmail.com>
2022-09-13 10:18:56 -0700

    setup

---
+++ src/components/PostList.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const PostList = () => {
+
+    return (
+        <div className='container'>
+             <h3>Post List</h3>
+        </div>
+    )
+}
+
+export default PostList
---
+++ src/components/UserHeader.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+const UserHeader = () => {
+
+    return (
+        <div className='container'>
+             <h3>AuthorName</h3>
+        </div>
+    )
+}
+
+export default UserHeader
